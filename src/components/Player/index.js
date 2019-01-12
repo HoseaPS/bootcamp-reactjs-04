842e821cd08b43bcf2335da874f61af1e9f92b60
Oséias P. Silva <hoseaps@gmail.com>
2019-01-12 11:55:57 -0200

    player styled

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
-import { Container, Current, Volume } from './styles';
+import Slider from 'rc-slider';
+
+import {
+  Container, Current, Volume, Progress, Controls, Time, ProgressSlider,
+} from './styles';
+
 import VoumeIcon from '../../assets/images/volume.svg';
+import ShuffleIcon from '../../assets/images/shuffle.svg';
+import BackwardIcon from '../../assets/images/backward.svg';
+import PlayIcon from '../../assets/images/play.svg';
+import PauseIcon from '../../assets/images/pause.svg';
+import ForwardIcon from '../../assets/images/forward.svg';
+import RepeatIcon from '../../assets/images/repeat.svg';
 
 const Player = () => (
   <Container>
@@ -14,8 +25,50 @@ const Player = () => (
         <small>Foo Fighters</small>
       </div>
     </Current>
+
+    <Progress>
+      <Controls>
+        <button type="button">
+          <img src={ShuffleIcon} alt="Suffle" />
+        </button>
+        <button type="button">
+          <img src={BackwardIcon} alt="Backward" />
+        </button>
+        <button type="button">
+          <img src={PlayIcon} alt="Play" />
+        </button>
+        <button type="button">
+          <img src={PauseIcon} alt="Pause" />
+        </button>
+        <button type="button">
+          <img src={ForwardIcon} alt="Forward" />
+        </button>
+        <button type="button">
+          <img src={RepeatIcon} alt="Repeat" />
+        </button>
+      </Controls>
+
+      <Time>
+        <span>1:39</span>
+        <ProgressSlider>
+          <Slider
+            railStyle={{ background: '#404040', borderRadius: 10 }}
+            trackStyle={{ background: '#1ed760' }}
+            handleStyle={{ border: 0 }}
+          />
+        </ProgressSlider>
+        <span>4:24</span>
+      </Time>
+    </Progress>
+
     <Volume>
       <img src={VoumeIcon} alt="Volume" />
+      <Slider
+        railStyle={{ background: '#404040', borderRadius: 10 }}
+        trackStyle={{ background: '#fff' }}
+        handleStyle={{ display: 'none' }}
+        value={100}
+      />
     </Volume>
   </Container>
 );

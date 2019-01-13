24f048ce50f1269b0006d2aa6a9cf57a78f97f22
Oséias P. Silva <hoseaps@gmail.com>
2019-01-13 13:12:49 -0200

    configuring redux and saga

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import React, { Fragment } from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+
+import './config/reactotron';
 import GlobalStyle from './styles/global';
+
 import Sidebar from './components/Sidebar';
 import Player from './components/Player';
 import Header from './components/Header';
+
 import { Wrapper, Container, Content } from './styles/components';
+
 import Routes from './routes';
+import store from './store';
 
 const App = () => (
-  <BrowserRouter>
-    <Fragment>
-      <GlobalStyle />
-      <Wrapper>
-        <Container>
-          <Sidebar />
-          <Content>
-            <Header />
-            <Routes />
-          </Content>
-        </Container>
-        <Player />
-      </Wrapper>
-    </Fragment>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Fragment>
+        <GlobalStyle />
+        <Wrapper>
+          <Container>
+            <Sidebar />
+            <Content>
+              <Header />
+              <Routes />
+            </Content>
+          </Container>
+          <Player />
+        </Wrapper>
+      </Fragment>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
